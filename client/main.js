3f21f4b0f175df34939e1f7837a9425100632e73
Mars Ballantyne <mars.ballantyne@gmail.com>
2019-02-15 12:49:19 -0500

    google config set

--- client/main.js
+++ client/main.js
@@ -29,3 +29,15 @@ Template.body.helpers({
     },
 });
 
+ServiceConfiguration.configurations.upsert(
+    {
+  service: "google"},
+    {$set: {
+        
+  clientId: "963060634925-n06db9kgl9qculfbvfsdnm0r21hflquf.apps.googleusercontent.com",
+  loginStyle: "popup",
+  secret: "4xRxDbowCA0Slcx387woBjPq"
+    }
+}
+);
+
--- system/service.js
+++
@@ -1,9 +0,0 @@
-ServiceConfiguration.configurations.remove({
-  service: "google"
-});
-ServiceConfiguration.configurations.insert({
-  service: "google",
-  clientId: "963060634925-n06db9kgl9qculfbvfsdnm0r21hflquf.apps.googleusercontent.com",
-  loginStyle: "popup",
-  secret: "4xRxDbowCA0Slcx387woBjPq"
-});
