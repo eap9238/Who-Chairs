0430aefd1c9bed5c6496a158ee0a4c5a31fbe693
lhb5405 <lhb5405@g.rit.edu>
2019-02-15 10:15:01 -0500

    Working example from tutorial

--- client/main.js
+++ client/main.js
@@ -5,28 +5,5 @@ import { render } from 'react-dom';
 import App from '../imports/ui/App.js';
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById('render-target'));
+    render(<App />, document.getElementById('render-target'));
 });
-
-/*
-import './main.html';
-import './views/comments/comments.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
-*/
--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,33 +1,61 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { withTracker } from 'meteor/react-meteor-data';
+
+import { Tasks } from '../api/tasks.js';
 
 import Task from './Task.js';
 
-export default class App extends Component {
-  getTasks() {
-    return [
-      { _id: 1, text: 'This is task 1'},
-      { _id: 2, text: 'This is task 2' },
-      { _id: 3, text: 'This is task 3' },
-    ];
-  }
+// App component - represents the whole app
+class App extends Component {
+    handleSubmit(event) {
+        event.preventDefault();
 
-  renderTasks() {
-    return this.getTasks().map((task) => (
-      <Task key={task._id} task={task} />
-    ));
-  }
+        // Find the text field via the React ref
+        const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
 
-  render() {
-    return (
-      <div className="container">
-        <header>
-          <h1>Todo List</h1>
-        </header>
+        Tasks.insert({
+            text,
+            createdAt: new Date(), // current time
+        });
 
-        <ul>
-          {this.renderTasks()}
-        </ul>
-      </div>
-    );
-  }
+        // Clear form
+        ReactDOM.findDOMNode(this.refs.textInput).value = '';
+    }
+
+    renderTasks() {
+        return this.props.tasks.map((task) => (
+            <Task key={task._id} task={task} />
+        ));
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <header>
+                    <h1>Todo List</h1>
+                </header>
+
+
+                <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
+                    <input
+                        type="text"
+                        ref="textInput"
+                        placeholder="Type to add new tasks"
+                    />
+                </form>
+
+                <ul>
+                    {this.renderTasks()}
+                </ul>
+            </div>
+        );
+    }
 }
+
+
+export default withTracker(() => {
+    return {
+        tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    };
+})(App);
