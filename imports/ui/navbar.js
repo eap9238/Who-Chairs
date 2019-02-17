c1620e21b174f672e47c19b4061be7374af903ac
ijo5372 <ijo5372@g.rit.edu>
2019-02-17 17:09:27 -0500

    All this is broken so i'm starting over.

---
+++ imports/ui/navbar.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav class="header-background navbar navbar-default" role="navigation">
+        <div class="container">
+          <div class="navbar-collapse collapse" id="nav-collapse">
+
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
