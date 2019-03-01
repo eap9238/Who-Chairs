3fe719a276cc8e8ffb1859b31dca4e1b894301c5
lhb5405 <lhb5405@g.rit.edu>
2019-03-01 17:22:09 -0500

    Removed leftover template code

--- client/main.js
+++ client/main.js
@@ -9,25 +9,6 @@ import './main.html';
 import '../lib/router.js';
 import './map.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
-
 Template.NavBar.helpers({
     user (){
         console.log(Meteor.user());
